fd537ab64ad76db3af9cbce44f61f069cb78e06b
DKIV9570 <53559717+DKIV9570@users.noreply.github.com>
2021-03-30 00:29:04 +0800

    Add files via upload
    fixed constructor

--- pa3/sqtree.cpp
+++ pa3/sqtree.cpp
@@ -47,52 +47,66 @@ SQtree::SQtree(PNG &imIn, double tol) {
     int h = imIn.height();
     stats s(imIn);
     pair<int, int> ul = make_pair(0, 0);
-    root = buildTree(s, ul, w, h, tol);
 
+    root = buildTree(s, ul, w, h, tol);
 }
 
 /**
  * Helper for the SQtree constructor.
  */
-SQtree::Node *SQtree::buildTree(stats &s, pair<int, int> &ul,
-                                int w, int h, double tol) {
+SQtree::Node * SQtree::buildTree(stats & s, pair<int,int> & ul,
+                                 int w, int h, double tol) {
 
-    RGBAPixel avg = s.getAvg(ul, w, h);
     Node *node = new Node(s, ul, w, h);
-
-    double minAvgVar;
     pair<int, int> splitter;
+    double minAvgVar = 10000000;
 
-    if (s.getVar(ul, w, h) <= tol || (w == 1 && h == 1)) return node;
 
-    for (int x = ul.first + 1; x <= ul.first + w; x++) {
-        for (int y = ul.second + 1; y <= ul.second + h; y++) {
-            double varNW = s.getVar(ul, x, y);
-            double varNE = s.getVar(make_pair(x - 1, ul.second), w - x, y);
-            double varSW = s.getVar(make_pair(ul.first, y - 1), x, h - y);
-            double varSE = s.getVar(make_pair(x - 1, y - 1), w - x, h - y);
+    if (s.getVar(ul, w, h) > tol || (w == 1 && h == 1))
+    {
+        cout << "returned" << endl;
+        return node;
+    }
 
-            double avgVar = (varNW + varNE + varSW + varSE) / 4;
+    for (int x = ul.first; x < ul.first + w; x++) {
+        for (int y = ul.second; y < ul.second + h; y++) {
+            if(!(x == 0 && y == 0)){
+                double varNW = s.getVar(ul, x-ul.first+1, y-ul.second+1);
+                double varNE = s.getVar(make_pair(x + 1, ul.second), w - x-1, y-ul.second+1);
+                double varSW = s.getVar(make_pair(ul.first, y + 1),x-ul.first+1, h-y-1);
+                double varSE = s.getVar(make_pair(x + 1, y + 1), w-x-1, h-y-1);
 
-            if (avgVar < minAvgVar) {
-                minAvgVar = avgVar;
-                splitter.first = x;
-                splitter.second = y;
+                double avgVar = (varNW + varNE + varSW + varSE) / 4;
+
+                if (avgVar < minAvgVar) {
+                    minAvgVar = avgVar;
+                    splitter.first = x;
+                    splitter.second = y;
+                }
             }
         }
     }
 
-    node->NW = buildTree(s, ul, splitter.first, splitter.second, tol);
-
-    pair<int, int> ul2 = make_pair(splitter.first - 1, ul.second);
-    node->NE = buildTree(s, ul2, w - splitter.first, splitter.second, tol);
-
-    pair<int, int> ul3 = make_pair(ul.first, splitter.second - 1);
-    node->SW = buildTree(s, ul3, splitter.first, h - splitter.second, tol);
+    cout<<splitter.first<<" "<<splitter.second<<endl;
 
     pair<int, int> ul4 = make_pair(splitter.first - 1, splitter.second - 1);
-    node->SE = buildTree(s, ul4, w - splitter.first, h - splitter.second,
-                         tol);
+    node->SE = buildTree(s, ul4, w - splitter.first, h - splitter.second, tol);
+
+    if(splitter.first != 0){
+        pair<int, int> ul3 = make_pair(ul.first, splitter.second - 1);
+        node->SW = buildTree(s, ul3, splitter.first, h - splitter.second, tol);
+    }
+
+    if(splitter.second != 0){
+        pair<int, int> ul2 = make_pair(splitter.first - 1, ul.second);
+        node->NE = buildTree(s, ul2, w - splitter.first, splitter.second, tol);
+    }
+
+    if(splitter.first != 0 && splitter.second != 0){
+        node->NW = buildTree(s, ul, splitter.first, splitter.second, tol);
+    }
+
+    return node;
 
 }
 
@@ -113,7 +127,7 @@ PNG SQtree::renderHelper(Node *curr, PNG &img) {
         if (curr->NW == NULL && curr->NE == NULL && curr->SW == NULL && curr->SE == NULL) {
             for (int x = curr->upLeft.first; x < curr->upLeft.first + curr->width; x++) {
                 for (int y = curr->upLeft.second; y < curr->upLeft.second + curr->height; y++) {
-                    RGBAPixel *p = img.getPixel(x,y);
+                    RGBAPixel *p = img.getPixel(x, y);
                     p = &curr->avg;
                 }
             }
@@ -134,13 +148,14 @@ void SQtree::clear() {
    clear_helper(root);
 }
 
-void SQtree::clear_helper(Node *curr){
-    if(curr != NULL){
+void SQtree::clear_helper(Node *curr) {
+    if (curr != NULL) {
         clear_helper(curr->NW);
         clear_helper(curr->NE);
         clear_helper(curr->SE);
         clear_helper(curr->SW);
-        delete(curr);
+        delete (curr);
+        curr = NULL;
     }
 }
 
@@ -148,18 +163,18 @@ void SQtree::copy(const SQtree &other) {
     copy_helper(other.root, root);
 }
 
-void SQtree::copy_helper(Node* other, Node* curr) {
+void SQtree::copy_helper(Node *other, Node *curr) {
     curr = other;
-    if(other->NW != NULL){
+    if (other->NW != NULL) {
         copy_helper(other->NW, curr->NW);
     }
-    if(other->NE != NULL){
+    if (other->NE != NULL) {
         copy_helper(other->NE, curr->NE);
     }
-    if(other->SE != NULL){
+    if (other->SE != NULL) {
         copy_helper(other->SE, curr->SE);
     }
-    if(other->SW != NULL){
+    if (other->SW != NULL) {
         copy_helper(other->SW, curr->SW);
     }
 }
